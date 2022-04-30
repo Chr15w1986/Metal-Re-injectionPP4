9063b86ca0b61b20f70ffdc2d5ae21e0a6a87e05
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-04-30 11:53:26 +0000

    remove http:// from allowed hosts causing heroku error

--- metal_reinjection/settings.py
+++ metal_reinjection/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://metal-re-injection.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['metal-re-injection.herokuapp.com', 'localhost']
 
 
 # Application definition
