6e92ccb8af07aecca349723f427b889e0d9c0364
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-08 21:09:21 +0000

    adaccounts d allauth to urlpatterns

--- metal_reinjection/urls.py
+++ metal_reinjection/urls.py
@@ -1,16 +1,16 @@
 """metal_reinjection URL Configuration"""
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', include('home.urls'), name='home-urls'),
-    path('posts/', include('posts.urls'), name='posts-urls')
+    path('posts/', include('posts.urls'), name='posts-urls'),
 ]
 
 
-handler404 = "metal_reinjection.views.page_not_found_view"
-handler500 = "metal_reinjection.views.custom_500_error_view"
+HANDLER_404 = "metal_reinjection.views.page_not_found_view"
+HANDLER_500 = "metal_reinjection.views.custom_500_error_view"
