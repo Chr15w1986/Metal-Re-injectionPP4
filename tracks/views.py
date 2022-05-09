fa9f585f643f0db49c2f57b0ef269acecb3518b4
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-09 21:12:14 +0000

    fix issue with form not posting (reverse_lazy)

--- tracks/views.py
+++ tracks/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, reverse
-from django.views.generic import CreateView
-from .forms import SongForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import SongForm
+from .models import Song
+
 
 class AddSong(CreateView, LoginRequiredMixin):
     form_class = SongForm
     template_name = 'tracks/add-song.html'
-    
+    success_url = reverse_lazy('song-list')
+
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class SongList(ListView):
+    model = Song
+    template_name = 'tracks/song-list.html'
+    context_object_name = 'songs'
+    queryset = Song.objects.all().order_by('title')
