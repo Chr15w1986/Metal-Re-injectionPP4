48b65c226d9a385c04d271a4edbb7b5a2f30ffb8
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-10 22:31:48 +0000

    remove commented out code and add docstring

--- tracks/models.py
+++ tracks/models.py
@@ -1,3 +1,4 @@
+""" Models for song-list """
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,12 +7,10 @@ class Song(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False)
     artist = models.CharField(max_length=200, null=False, blank=False)
     original_artist = models.CharField(max_length=200, null=False, blank=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, related_name='author')
+    author = models.ForeignKey(
+                User, on_delete=models.CASCADE, null=False, blank=False,
+                related_name='author')
     url = models.URLField(null=False, blank=False)
 
     def __str__(self):
         return str(self.title)
-    
-    # def save(self, *args, **kwargs):
-    #     self.author = request.user
-    #     super(Song, self).save(*args, **kwargs)
