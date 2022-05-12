d6c429f87451bb318100552019024cd84fececac
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-12 22:18:22 +0000

    add url path for single-song

--- tracks/urls.py
+++ tracks/urls.py
@@ -5,4 +5,6 @@ APP_NAME = 'tracks'
 urlpatterns = [
     path('addsong/', views.AddSong.as_view(), name='add-song'),
     path('song-list/', views.SongList.as_view(), name='song-list'),
+    path('single-song/<int:pk>/', views.SingleSongDetail.as_view(),
+         name='single-song'),
 ]
