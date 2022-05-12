370db9b29e6db2ea26a6245f6785c32b9093c0c9
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-12 22:29:04 +0000

    move summernote admin import above .models -pylint

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -22,5 +22,3 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
-
-
