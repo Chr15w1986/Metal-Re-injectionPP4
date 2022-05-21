58aebd1211f9efe75561ffcdfdc3fe2ce40b2e19
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-21 20:01:44 +0000

    fix 404 and 500 filepath issue

--- metal_reinjection/views.py
+++ metal_reinjection/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 
 
 def page_not_found_view(request, exception):
-    return render(request, '404.html', status=404)
+    data = {}
+    return render(request, 'templates/404.html', data)
 
 
-def custom_500_error_view(request, *args, **argv):
-    return render(request, '500.html', status=500)
+def custom_500_error_view(request,  exception):
+    data = {}
+    return render(request, 'templates/500.html', data)
