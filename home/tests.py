ff658b6f1bb1ba6276f3d6186740583a78443804
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-24 20:16:34 +0000

    create tests for home views

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestLandingPageViews(TestCase):
+
+    def test_landing_page_status_code_is_ok(self):
+        home_page = self.client.get('/')
+        self.assertEqual(home_page.status_code, 200)
+
+    def test_landing_page_status_code_is_not_ok(self):
+        home_page = self.client.get('/')
+        self.assertNotEqual(home_page.status_code, 403)
