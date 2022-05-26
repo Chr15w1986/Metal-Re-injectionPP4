5713a5dfa394f550992c4b98cd46f83a9791e6d5
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-26 19:05:56 +0000

    create file for parsing embedded spotify url

---
+++ tracks/spotify_tools.py
@@ -0,0 +1,21 @@
+from urllib.parse import urlparse
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
+def test_and_rebuild_link(url_to_test):
+
+    try:
+        track = sp.track(url_to_test)
+        original_track_url = track['external_urls']['spotify']
+        parsed_url = urlparse(original_track_url)
+        path = parsed_url.path
+        rebuilt_url = parsed_url._replace(path='embed'+path).geturl()
+        return rebuilt_url
+
+    except spotipy.exceptions.SpotifyException:
+        return False
