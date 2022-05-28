b3161151215191cb1354ad53d72e6abcdd398c09
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-28 08:53:37 +0000

    add clean url function to songform, update docstrings

--- tracks/forms.py
+++ tracks/forms.py
@@ -1,9 +1,16 @@
+""" Forms module """
+
 from django.forms import ModelForm
+from django.core.exceptions import ValidationError
 from .models import Song
+from .spotify_tools import test_and_rebuild_link
 
 
 class SongForm(ModelForm):
+    """ Creates a form for the user to add a new song to the database,
+        including user input url validation"""
     class Meta:
+        """ defines how the SongForm class behaves """
         model = Song
         fields = [
             'title',
@@ -11,3 +18,12 @@ class SongForm(ModelForm):
             'original_artist',
             'url'
         ]
+
+    def clean_url(self):
+        """ Cleans and rebuilds user url input within the form,
+            including validation for incorrect input """
+        url = self.cleaned_data['url']
+        rebuilt_url = test_and_rebuild_link(url)
+        if not rebuilt_url:
+            raise ValidationError("Sorry, that's not a valid Spotify link!")
+        return rebuilt_url
