60ae5c2d4841d270ef6beaaf301366e7eb4dce93
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-28 08:38:30 +0000

    add class/module docstrings, test update/delete classes

--- tracks/views.py
+++ tracks/views.py
@@ -1,13 +1,15 @@
-from urllib.parse import urljoin, urlparse, urlsplit
+""" Views for tracks app """
+
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import SongForm
 from .models import Song
 
 
 class SongList(ListView):
+    """ Songlist view for all tracks (Track list)"""
     model = Song
     template_name = 'tracks/song-list.html'
     context_object_name = 'songs'
@@ -16,6 +18,7 @@ class SongList(ListView):
 
 
 class UserSongList(ListView):
+    """ Usersonglist view for user added tracks (My tracks)"""
     template_name = 'tracks/user-songs.html'
     model = Song
     context_object_name = 'usersongs'
@@ -27,14 +30,11 @@ class UserSongList(ListView):
 
 
 class AddSong(CreateView, LoginRequiredMixin):
+    """ Addsong form view to allow users to add tracks (Add song)
+         while logged in"""
     form_class = SongForm
     template_name = 'tracks/add-song.html'
     success_url = reverse_lazy('song-list')
-    # urlparse('https://open.spotify.com/')
-    urlsplit('https://open.spotify.com/')
-    urljoin('https://open.spotify.com/', 'embed/track/')
-    print(urlparse('https://open.spotify.com/'))
-    print(urlsplit('https://open.spotify.com/'))
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -42,19 +42,33 @@ class AddSong(CreateView, LoginRequiredMixin):
 
 
 class SingleSongDetail(DetailView):
+    """ Singlesong view to allow users to listen to
+         and rate tracks (Rate a Track)"""
     model = Song
     template_name = 'tracks/single-song.html'
     context = {'song': Song}
 
 
-class DeleteSong(DeleteView):
+class DeleteSong(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """ Deletesong view to allow logged in users to remove their
+         own addition from the database"""
     model = Song
     template_name = 'tracks/delete-song.html'
     success_url = reverse_lazy('song-list')
 
+    def test_func(self):
+        self.object = self.get_object()
+        return self.object.author == self.request.user
 
-class UpdateSong(UpdateView):
+
+class UpdateSong(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """ Updatesong view to allow logged in users to edit their
+     own addition in the database"""
     model = Song
     form_class = SongForm
     template_name = 'tracks/update-song.html'
     success_url = reverse_lazy('song-list')
+
+    def test_func(self):
+        self.object = self.get_object()
+        return self.object.author == self.request.user
