076b8177c669dca4496b137df74b4cd884e3b2b9
Chr15w1986 <chr15w1986@hotmail.co.uk>
2022-05-28 14:04:44 +0000

    remove unused code

--- metal_reinjection/settings.py
+++ metal_reinjection/settings.py
@@ -161,7 +161,6 @@ EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-# DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
